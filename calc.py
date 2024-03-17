14a3c61ff0ef1479a6e0afefda1bf297b95a16ba
Lee-Seungje <moondgod@gmail.com>
2024-03-17 15:10:10 +0900

    Add calc

---
+++ calc.py
@@ -0,0 +1,9 @@
+def calcNum():
+    num1 =  int(input('첫 번째 숫자를 입력하세요 : '))
+    num2 =  int(input('두 번째 숫자를 입력하세요 : '))
+    print("{} + {} = {}".format(num1,num2,num1+num2))
+    print("{} - {} = {}".format(num1,num2,num1-num2))
+    print("{} * {} = {}".format(num1,num2,num1*num2))
+    print("{} / {} = {}".format(num1,num2,num1/num2))
+
+calcNum();
